6dcf38adead5a2e4916327982ce8cc15994388ac
kudou-reira <h.i.y.e.o.xd@gmail.com>
2020-10-19 10:01:29 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.62b3d631b89f4790fea6d3ccfecfc161.js"
+  "/portfolio/precache-manifest.a4e2992ac348df5e07ad4df202029904.js"
 );
 
 self.addEventListener('message', (event) => {
