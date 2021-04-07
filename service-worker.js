e4204338a0ac33b49d4afc0076f28e5afce8af3d
kudou-reira <h.i.y.e.o.xd@gmail.com>
2021-04-07 15:50:37 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1c541169ca29e41cf9cee884e8279e61.js"
+  "/portfolio/precache-manifest.5da910857ac5fe71665f788849b4f034.js"
 );
 
 self.addEventListener('message', (event) => {
